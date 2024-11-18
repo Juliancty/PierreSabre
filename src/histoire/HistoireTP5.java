34a8d2068e9c619b15b09b55964243051f8d2129
Julian Cutaya <ctj5223a@ensfsi.onmicrosoft.com>
2024-11-18 17:02:16 +0100

    les_samourais

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -10,13 +10,8 @@ public class HistoireTP5 {
 		Commercant kumi = new Commercant("Kumi", 10);
 		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 		Ronin roro = new Ronin("Roro", "shochu", 60);
+		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
 		
-		marco.faireConnaissance(roro);
-		marco.faireConnaissance(yaku);
-		marco.faireConnaissance(chonin);
-		marco.faireConnaissance(kumi);
-		marco.listerConnaissance();
-		roro.listerConnaissance();
-		yaku.listerConnaissance();
+		akimoto.boire("thé");
 	}
 }
