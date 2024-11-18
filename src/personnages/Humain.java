6f8ea69197a5c00442106972acf3a9d8cefc0877
Julian Cutaya <ctj5223a@ensfsi.onmicrosoft.com>
2024-11-18 16:39:16 +0100

    les_humains_ont_des_connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -6,6 +6,8 @@ public class Humain {
 	private String nom;
 	private String boissonFav;
 	protected int argent;
+	private int nbConnaissance = 0;
+	private Humain[] memoire = new Humain[30];
 	
 	public Humain(String nom, String boissonFav, int argent) {
 		this.nom = nom;
@@ -52,4 +54,49 @@ public class Humain {
 		}
 	}
 	
+	public void memoriser(Humain autreHumain) {
+		if(this.nbConnaissance < 30) {
+			this.memoire[this.nbConnaissance] = autreHumain;
+			this.nbConnaissance++;
+		} else {
+			for(int i = 0; i < 29; i++) {
+				this.memoire[i] = this.memoire[i+1];
+			}
+			this.memoire[29] = autreHumain;
+		}
+	}
+	
+	public void repondre(Humain autreHumain) {
+		direBonjour();
+		memoriser(autreHumain);
+	}
+	
+	public void faireConnaissance(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+		
+	}
+	
+	public void listerConnaissance() {
+		String texte = "Je connais beaucoup de monde dont :";
+		for(int i = 0; i < this.nbConnaissance-1; i++) {
+			texte += " " + this.memoire[i].getNom() + ',';
+		}
+		texte += " " + this.memoire[this.nbConnaissance-1].getNom();
+		parler(texte);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
